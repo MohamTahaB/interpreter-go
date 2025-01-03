696fcae835e94a59a6f84116d591216e8a1bb8f7
MohamTahaB <bayoumimt@gmail.com>
2025-01-03 12:12:39 +0100

    init-ast-parser
    Initiate the abstract structure tree and the parser

---
+++ parser/parser.go
@@ -0,0 +1,97 @@
+package parser
+
+import (
+	"github.com/MohamTahaB/interpreter-go/ast"
+	"github.com/MohamTahaB/interpreter-go/lexer"
+	"github.com/MohamTahaB/interpreter-go/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	currToken token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{
+		l: l,
+	}
+
+	// nextToken twice to populate both current and peek tokens
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.currToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+
+	for p.currToken.Type != token.EOF {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		p.nextToken()
+	}
+
+	return program
+}
+
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.currToken.Type {
+	case token.LET:
+		return p.parseLetStatement()
+	default:
+		return nil
+	}
+}
+
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+	stmt := &ast.LetStatement{
+		Token: p.currToken,
+	}
+
+	if !p.expectPeek(token.IDENT) {
+		return nil
+	}
+
+	stmt.Name = &ast.Identifier{
+		Token: p.currToken,
+		Value: p.currToken.Literal,
+	}
+
+	if !p.expectPeek(token.ASSIGN) {
+		return nil
+	}
+
+	// TODO: the expression block to be collected afterwards, basically the same thing until we get to the semicolon;
+	for !p.currTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+func (p *Parser) expectPeek(tokType token.TokenType) bool {
+	if p.peekTokenIs(tokType) {
+		p.nextToken()
+		return true
+	}
+	return false
+}
+
+func (p *Parser) peekTokenIs(tokType token.TokenType) bool {
+	return p.peekToken.Type == tokType
+}
+
+func (p *Parser) currTokenIs(tokType token.TokenType) bool {
+	return p.currToken.Type == tokType
+}
